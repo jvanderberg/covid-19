4a6f94e8f4467fb370cbf74b6e435f30d7028bbd
Josh Vanderberg <josh@yesenergy.com>
2020-09-27 03:40:36 +0000

    09/26 and north/south breakdown

--- county.py
+++ county.py
@@ -4,85 +4,87 @@ import pandas as pd
 import numpy as np
 import requests
 from scipy import stats
-r = requests.get("https://www.dph.illinois.gov/sitefiles/COVIDHistoricalTestResults.json?nocache=5")
-county_json= r.text
-regions = pd.read_csv('regions.csv')
-data = json.loads(county_json)
 
-table = {}
-table['date'] = []
-table['county'] = []
-table['deaths'] = []
-table['count'] = []
-table['tested'] = []
-table['percentage'] = []
-table['deaths_per_million'] = []
-table['deaths_7day'] = []
-table['count_7day'] = []
-table['tested_7day'] = []
-table['percentage_7day'] = []
-table['deaths_per_million_7day'] = []
-table['deaths_14day'] = []
-table['count_14day'] = []
-table['tested_14day'] = []
-table['percentage_14day'] = []
-table['deaths_per_million_14day'] = []
+def get_regional_breakdown(region_file):
+    r = requests.get("https://www.dph.illinois.gov/sitefiles/COVIDHistoricalTestResults.json?nocache=5")
+    county_json= r.text
+    regions = pd.read_csv(region_file)
+    data = json.loads(county_json)
 
-startdate=datetime.datetime(2020,4,19)
-target_zips = ['60301', '60302', '60304']
+    table = {}
+    table['date'] = []
+    table['county'] = []
+    table['deaths'] = []
+    table['count'] = []
+    table['tested'] = []
+    table['percentage'] = []
+    table['deaths_per_million'] = []
+    table['deaths_7day'] = []
+    table['count_7day'] = []
+    table['tested_7day'] = []
+    table['percentage_7day'] = []
+    table['deaths_per_million_7day'] = []
+    table['deaths_14day'] = []
+    table['count_14day'] = []
+    table['tested_14day'] = []
+    table['percentage_14day'] = []
+    table['deaths_per_million_14day'] = []
 
-for date_data in data['historical_county']['values']:
-    date = date_data['testDate']
-    for county in date_data['values']:
-        table['date'].append(date)
-        table['county'].append(county['County'])
-        table['tested'].append(county['total_tested'])
-        table['count'].append(county['confirmed_cases'])
-        table['deaths'].append(county['deaths'])
-        table['percentage'].append(0)
-        table['deaths_per_million'].append(0)
-        table['tested_7day'].append(0)
-        table['count_7day'].append(0)
-        table['deaths_7day'].append(0)
-        table['percentage_7day'].append(0)
-        table['deaths_per_million_7day'].append(0)
-        table['tested_14day'].append(0)
-        table['count_14day'].append(0)
-        table['deaths_14day'].append(0)
-        table['percentage_14day'].append(0)
-        table['deaths_per_million_14day'].append(0)
+    for date_data in data['historical_county']['values']:
+        date = date_data['testDate']
+        for county in date_data['values']:
+            table['date'].append(date)
+            table['county'].append(county['County'])
+            table['tested'].append(county['total_tested'])
+            table['count'].append(county['confirmed_cases'])
+            table['deaths'].append(county['deaths'])
+            table['percentage'].append(0)
+            table['deaths_per_million'].append(0)
+            table['tested_7day'].append(0)
+            table['count_7day'].append(0)
+            table['deaths_7day'].append(0)
+            table['percentage_7day'].append(0)
+            table['deaths_per_million_7day'].append(0)
+            table['tested_14day'].append(0)
+            table['count_14day'].append(0)
+            table['deaths_14day'].append(0)
+            table['percentage_14day'].append(0)
+            table['deaths_per_million_14day'].append(0)
 
-df = pd.DataFrame(table)
-df['date'] = pd.to_datetime(df['date'])
-df = pd.merge(df,regions,how='inner', on='county')
-df = df.groupby(by=['date','region']).sum()
-df = df.reset_index()
-df2 = df.pivot(index='date', columns='region')
+    df = pd.DataFrame(table)
+    df['date'] = pd.to_datetime(df['date'])
+    df = pd.merge(df,regions,how='inner', on='county')
+    df = df.groupby(by=['date','region']).sum()
+    df = df.reset_index()
+    df2 = df.pivot(index='date', columns='region')
 
-df2['deaths'] = df2['deaths'].diff(periods=1)
-df2['count'] = df2['count'].diff(periods=1)
-df2['tested'] = df2['tested'].diff(periods=1)
-df2['percentage'] = df2['count'] / df2['tested']
-df2.dropna(inplace=True)
-# Remove outliers and interpolate
-df2.loc[abs(stats.zscore(df2['deaths']['Illinois'])) > 3] = np.nan
-df2 = df2.interpolate().round()
+    df2['deaths'] = df2['deaths'].diff(periods=1)
+    df2['count'] = df2['count'].diff(periods=1)
+    df2['tested'] = df2['tested'].diff(periods=1)
+    df2.dropna(inplace=True)
+    # Remove outliers and interpolate
+    df2.loc[abs(stats.zscore(df2['deaths']['Illinois'])) > 3] = np.nan
+    df2 = df2.interpolate().round()
+    df2['percentage'] = df2['count'] / df2['tested']
+    df2['deaths_per_million'] = 1000000 * df2['deaths'] / df2['population']
 
-values = pd.DataFrame(df2)
+    values = pd.DataFrame(df2)
 
-df2['deaths_7day'] = df2['deaths'].rolling(window=7).mean()
-df2['count_7day'] = df2['count'].rolling(window=7).mean()
-df2['tested_7day'] = df2['tested'].rolling(window=7).mean()
-df2['percentage_7day'] = df2['count_7day'] / df2['tested_7day']
-df2['deaths_per_million_7day'] = 1000000 * df2['deaths_7day'] / df2['population']
+    df2['deaths_7day'] = df2['deaths'].rolling(window=7).mean()
+    df2['count_7day'] = df2['count'].rolling(window=7).mean()
+    df2['tested_7day'] = df2['tested'].rolling(window=7).mean()
+    df2['percentage_7day'] = df2['count_7day'] / df2['tested_7day']
+    df2['deaths_per_million_7day'] = 1000000 * df2['deaths_7day'] / df2['population']
 
-df2['deaths_14day'] = df2['deaths'].rolling(window=14).mean()
-df2['count_14day'] = df2['count'].rolling(window=14).mean()
-df2['tested_14day'] = df2['tested'].rolling(window=14).mean()
-df2['percentage_14day'] = df2['count_14day'] / df2['tested_14day']
-df2['deaths_per_million_14day'] = 1000000 * df2['deaths_14day'] / df2['population']
-
-df2.to_csv('regional_all.csv')
+    df2['deaths_14day'] = df2['deaths'].rolling(window=14).mean()
+    df2['count_14day'] = df2['count'].rolling(window=14).mean()
+    df2['tested_14day'] = df2['tested'].rolling(window=14).mean()
+    df2['percentage_14day'] = df2['count_14day'] / df2['tested_14day']
+    df2['deaths_per_million_14day'] = 1000000 * df2['deaths_14day'] / df2['population']
+    return df2
+    
+get_regional_breakdown('regions.csv').to_csv('regional_all.csv')
+get_regional_breakdown('regions_north_south.csv').to_csv('regional_north_south.csv')
 
 ##############################################################################
 # Calculate hospitalization stats
--- regional_charts.py
+++ regional_charts.py
@@ -228,3 +228,81 @@ plt.gca().xaxis.set_major_locator(mdates.MonthLocator(interval=1))
 
 plt.savefig('IL Hospitalization.png', bbox_inches='tight')
 plt.close()
+
+
+df = pd.read_csv('regional_north_south.csv', header=[0,1], index_col=0, parse_dates=True)
+
+#######################################################################################
+# North/South Positive %
+#######################################################################################
+items = [ 'Illinois', 'North', 'South']
+
+plt.close()
+plt.figure(1, dpi=800, clear=True)
+f, ax = plt.subplots(3, sharex=True, sharey=True)
+june_plus = df[df.index > datetime.datetime(2020,5,31)]
+for col in range(0,3):
+    item = items[col]
+    current_value = df.tail(1)['percentage_7day'][item][0]
+
+    ax[col].plot(june_plus.index, june_plus['percentage_7day'][item], color='#6688cc', linewidth=3)
+    ax[col].set_title(item +' - {:0.1f}%'.format(100*current_value),fontsize=18)
+    ax[col].grid(axis='y', linewidth=0.5)
+    ax[col].set_ylim(0,.12)
+    ax[col].spines["top"].set_visible(False)
+    ax[col].spines["right"].set_visible(False)
+    ax[col].spines["left"].set_visible(False)
+    ax[col].spines["bottom"].set_visible(False)
+    ax[col].yaxis.set_major_formatter(mtick.PercentFormatter(1.0))
+    ax[col].xaxis.set_major_formatter(mdates.DateFormatter('%m/%d')) 
+    ax[col].xaxis.set_major_locator(mdates.MonthLocator(interval=1)) 
+
+f.set_figheight(16)
+f.set_figwidth(8)
+f.tight_layout(pad=6, w_pad=1, h_pad=5)
+# Format the date into months & days
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d')) 
+plt.gca().yaxis.set_major_formatter(mtick.PercentFormatter(1.0)) 
+
+# Change the tick interval
+plt.gca().xaxis.set_major_locator(mdates.MonthLocator(interval=1)) 
+plt.box(False)
+
+#plt.margins(0)
+f.suptitle('Illinois North/South % Positive - 7 Day Average', fontsize=18)
+
+plt.savefig('North-South Percentage.png', dpi=400)
+
+#######################################################################################
+# North/South Deaths Per Million
+#######################################################################################
+
+plt.close()
+plt.figure(1, dpi=800, clear=True)
+f, ax = plt.subplots(3, sharex=False, sharey=True)
+june_plus = df[df.index > datetime.datetime(2020,4,30)]
+for col in range(0,3):
+    item = items[col]
+    current_value = df.tail(1)['deaths_per_million_7day'][item][0]
+
+    ax[col].plot(june_plus.index, june_plus['deaths_per_million_7day'][item], color='#6688cc', linewidth=3)
+    ax[col].set_title(item +' - {:0.1f}'.format(current_value), fontsize=18)
+    ax[col].grid(axis='y', linewidth=0.5)
+    ax[col].set_ylim(0,10)
+    ax[col].spines["top"].set_visible(False)
+    ax[col].spines["right"].set_visible(False)
+    ax[col].spines["left"].set_visible(False)
+    ax[col].spines["bottom"].set_visible(False)
+    ax[col].xaxis.set_major_formatter(mdates.DateFormatter('%m/%d')) 
+    ax[col].xaxis.set_major_locator(mdates.MonthLocator(interval=1)) 
+
+
+f.set_figheight(16)
+f.set_figwidth(8)
+f.tight_layout(pad=6, w_pad=1, h_pad=5)
+f.suptitle('Illinois North/South Death Rates Per Million - 7 Day Average', fontsize=18)
+
+plt.box(False)
+
+#plt.margins(0)
+plt.savefig('North-South deaths per Million.png', dpi=200)
