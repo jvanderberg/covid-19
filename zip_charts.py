5bb336adb3de6bb47e0dfc65babc05efa13045e2
Josh Vanderberg <josh@yesenergy.com>
2020-11-01 17:53:41 -0600

    11/1

--- zip_charts.py
+++ zip_charts.py
@@ -13,8 +13,8 @@ matplotlib.rcParams['axes.labelcolor'] = '#555555'
 matplotlib.rcParams['xtick.color'] = '#555555'
 matplotlib.rcParams['ytick.color'] = '#555555'
 
-df = pd.read_csv('zip_rollup_all.csv', header=[0], index_col=0, parse_dates=True)
-
+df = pd.read_csv('zip_rollup_all.csv', header=[
+                 0], index_col=0, parse_dates=True)
 
 
 #######################################################################################
@@ -24,7 +24,8 @@ plt.figure(figsize=(10, 5), dpi=400)
 plt.box(0)
 plt.margins(0)
 width = 0.75
-p1 = plt.bar(df.index, df['count'], width, color='#6688cc', label="Daily Positives")
+p1 = plt.bar(df.index, df['count'], width,
+             color='#6688cc', label="Daily Positives")
 plt.plot(df.index, df['count_14day'], color='#EE3333', label="14 Day Average")
 plt.ylabel("Daily Positives")
 plt.legend(loc='best')
@@ -32,12 +33,13 @@ plt.legend().get_frame().set_linewidth(0.0)
 current_value = df.tail(1)['count_14day'][0]
 current_date = df.tail(1)['count_14day'].index[0]
 plt.ylim(0)
-plt.title('Oak Park Daily Positive cases - '+'{:%b %-d} - {:0.1f} cases per day'.format(current_date,current_value))
+plt.title('Oak Park Daily Positive cases - ' +
+          '{:%b %-d} - {:0.1f} cases per day'.format(current_date, current_value))
 plt.grid(axis='y', linewidth=0.5)
-plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d')) 
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d'))
 
 # Change the tick interval
-plt.gca().xaxis.set_major_locator(mdates.MonthLocator(interval=1)) 
+plt.gca().xaxis.set_major_locator(mdates.MonthLocator(interval=1))
 
 plt.savefig('charts/Oak Park Positives.png', bbox_inches='tight')
 plt.close()
@@ -47,7 +49,8 @@ plt.close()
 # Positives by age group
 #######################################################################################
 
-age_groups = [{'group':'<20', 'color': 'tab:blue'}, {'group':'20-29', 'color': 'tab:orange'}, {'group':'30-39', 'color': 'tab:green'}, {'group': '40-49', 'color': 'tab:red'}, {'group': '50-59', 'color': 'tab:purple'}, {'group':'60-69', 'color': 'tab:brown'}, {'group':'70-79', 'color': 'tab:gray'}, {'group':'80+','color':'tab:cyan'} ]
+age_groups = [{'group': '<20', 'color': 'tab:blue'}, {'group': '20-29', 'color': 'tab:orange'}, {'group': '30-39', 'color': 'tab:green'}, {'group': '40-49', 'color': 'tab:red'},
+              {'group': '50-59', 'color': 'tab:purple'}, {'group': '60-69', 'color': 'tab:brown'}, {'group': '70-79', 'color': 'tab:gray'}, {'group': '80+', 'color': 'tab:cyan'}]
 age_groups.reverse()
 
 plt.figure(figsize=(10, 5), dpi=400)
@@ -56,35 +59,35 @@ plt.margins(0)
 x = df.index
 y = []
 for age in age_groups:
-    y.append(df[age['group']+' count_14day'])
-    #plt.plot(df.index, df[age['group']+' count_14day'], color=age['color'], linewidth=4, label=age['group'])
+    y.append(100*df[age['group']+' count_14day']/df['count_14day'])
 
-plt.stackplot(x,y, labels=[age['group'] for age in age_groups], alpha=0.75)
-#plt.plot(df.index, df['count_14day'], color='black', label='All Ages', linewidth=4 )
+plt.stackplot(x, y, labels=[age['group'] for age in age_groups], alpha=0.75)
 
-plt.ylabel("Daily Positives")
-plt.legend(loc='upper left', bbox_to_anchor=(1, 1)).get_frame().set_linewidth(0.0)
+plt.ylabel("Daily Positive Percentage of Total")
+plt.legend(loc='upper left', bbox_to_anchor=(
+    1, 1)).get_frame().set_linewidth(0.0)
 
 current_value = df.tail(1)['count_14day'][0]
 current_date = df.tail(1)['count_14day'].index[0]
 
-plt.title('Oak Park Cases by Age Group - '+'{:%b %-d} - 14 day average'.format(current_date))
+plt.title('Oak Park Cases by Age Group - ' +
+          '{:%b %-d} - 14 day average'.format(current_date))
 plt.grid(axis='y', linewidth=0.5)
-plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d')) 
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d'))
 
 # Change the tick interval
-plt.gca().xaxis.set_major_locator(mdates.MonthLocator(interval=1)) 
+plt.gca().xaxis.set_major_locator(mdates.MonthLocator(interval=1))
 
 plt.savefig('charts/Oak Park Positives by age.png', bbox_inches='tight')
 plt.close()
-
 
 
 #######################################################################################
 # Tests by age group
 #######################################################################################
 
-age_groups = [{'group':'<20', 'color': 'tab:blue'}, {'group':'20-29', 'color': 'tab:orange'}, {'group':'30-39', 'color': 'tab:green'}, {'group': '40-49', 'color': 'tab:red'}, {'group': '50-59', 'color': 'tab:purple'}, {'group':'60-69', 'color': 'tab:brown'}, {'group':'70-79', 'color': 'tab:gray'}, {'group':'80+','color':'tab:cyan'} ]
+age_groups = [{'group': '<20', 'color': 'tab:blue'}, {'group': '20-29', 'color': 'tab:orange'}, {'group': '30-39', 'color': 'tab:green'}, {'group': '40-49', 'color': 'tab:red'},
+              {'group': '50-59', 'color': 'tab:purple'}, {'group': '60-69', 'color': 'tab:brown'}, {'group': '70-79', 'color': 'tab:gray'}, {'group': '80+', 'color': 'tab:cyan'}]
 age_groups.reverse()
 
 plt.figure(figsize=(10, 5), dpi=400)
@@ -96,19 +99,21 @@ for age in age_groups:
     y.append(df[age['group']+' tested_14day'])
     #plt.plot(df.index, df[age['group']+' count_14day'], color=age['color'], linewidth=4, label=age['group'])
 
-plt.stackplot(x,y, labels=[age['group'] for age in age_groups], alpha=0.75)
+plt.stackplot(x, y, labels=[age['group'] for age in age_groups], alpha=0.75)
 #plt.plot(df.index, df['count_14day'], color='black', label='All Ages', linewidth=4 )
 
 plt.ylabel("Daily Tests")
-plt.legend(loc='upper left', bbox_to_anchor=(1, 1)).get_frame().set_linewidth(0.0)
+plt.legend(loc='upper left', bbox_to_anchor=(
+    1, 1)).get_frame().set_linewidth(0.0)
 
 
-plt.title('Oak Park Tests by Age Group - '+'{:%b %-d} - 14 day average'.format(current_date))
+plt.title('Oak Park Tests by Age Group - ' +
+          '{:%b %-d} - 14 day average'.format(current_date))
 plt.grid(axis='y', linewidth=0.5)
-plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d')) 
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d'))
 
 # Change the tick interval
-plt.gca().xaxis.set_major_locator(mdates.MonthLocator(interval=1)) 
+plt.gca().xaxis.set_major_locator(mdates.MonthLocator(interval=1))
 
 plt.savefig('charts/Oak Park Tests by age.png', bbox_inches='tight')
 plt.close()
@@ -121,20 +126,22 @@ plt.figure(figsize=(10, 5), dpi=400)
 plt.box(on=None)
 plt.margins(0)
 width = 0.75
-plt.plot(df.index, df['percentage_14day'], color='#6688cc', label="14 Day Average")
+plt.plot(df.index, df['percentage_14day'],
+         color='#6688cc', label="14 Day Average")
 plt.ylabel("% Positive")
 plt.legend(loc='best')
 plt.legend().get_frame().set_linewidth(0.0)
 current_date = df.tail(1)['percentage_14day'].index[0]
 current_value_percentage = df.tail(1)['percentage_14day'][0]
 
-plt.title('Oak Park Daily Positive % - '+'{:%b %-d} - {:0.1f}% positive'.format(current_date, 100* current_value_percentage))
+plt.title('Oak Park Daily Positive % - ' +
+          '{:%b %-d} - {:0.1f}% positive'.format(current_date, 100 * current_value_percentage))
 plt.grid(axis='y', linewidth=0.5)
-plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d')) 
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d'))
 
 # Change the tick interval
-plt.gca().xaxis.set_major_locator(mdates.MonthLocator(interval=1)) 
-plt.gca().yaxis.set_major_formatter(mtick.PercentFormatter(1.0)) 
+plt.gca().xaxis.set_major_locator(mdates.MonthLocator(interval=1))
+plt.gca().yaxis.set_major_formatter(mtick.PercentFormatter(1.0))
 plt.ylim(0)
 plt.savefig('charts/Oak Park Positive Pct.png', bbox_inches='tight')
 plt.close()
@@ -142,21 +149,23 @@ plt.close()
 #######################################################################################
 # Completed Test Report
 #######################################################################################
-plt.figure(1,figsize=(10, 5), dpi=400, clear=True)
+plt.figure(1, figsize=(10, 5), dpi=400, clear=True)
 width = 0.75
-p1 = plt.bar(df.index, df['tested'], width, color='#6688cc', label="Tests Completed")
+p1 = plt.bar(df.index, df['tested'], width,
+             color='#6688cc', label="Tests Completed")
 plt.plot(df.index, df['tested_14day'], color='#EE3333', label="7 Day Average")
 plt.ylabel("Daily Tests")
 current_value = df.tail(1)['tested_14day'].round()[0]
 current_date = df.tail(1)['tested_14day'].index[0]
-plt.title('Oak Park Tests Completed - '+'{:%b %-d} - {:,} test per day'.format(current_date,int(current_value)))
+plt.title('Oak Park Tests Completed - ' +
+          '{:%b %-d} - {:,} test per day'.format(current_date, int(current_value)))
 plt.grid(axis='y', linewidth=0.5)
 plt.ylim(0)
 # Format the date into months & days
-plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d')) 
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d'))
 
 # Change the tick interval
-plt.gca().xaxis.set_major_locator(mdates.MonthLocator(interval=1)) 
+plt.gca().xaxis.set_major_locator(mdates.MonthLocator(interval=1))
 plt.box(False)
 plt.legend()
 plt.legend().get_frame().set_linewidth(0.0)
@@ -166,15 +175,19 @@ plt.savefig('charts/Oak Park Testing.png', bbox_inches='tight')
 #######################################################################################
 # Positive testing % by zip
 #######################################################################################
-df = pd.read_csv('zip_detail_all.csv',  header=[0,1], index_col=0, parse_dates=True)
+df = pd.read_csv('zip_detail_all.csv',  header=[
+                 0, 1], index_col=0, parse_dates=True)
 
 plt.figure(figsize=(10, 5), dpi=400)
 plt.box(on=None)
 plt.margins(0)
 width = 0.75
-plt.plot(df.index, df['percentage_14day']['60301'], color='#6688cc', label="60301")
-plt.plot(df.index, df['percentage_14day']['60302'], color='#EE3333', label="60302")
-plt.plot(df.index, df['percentage_14day']['60304'], color='#33EE33', label="60304")
+plt.plot(df.index, df['percentage_14day']
+         ['60301'], color='#6688cc', label="60301")
+plt.plot(df.index, df['percentage_14day']
+         ['60302'], color='#EE3333', label="60302")
+plt.plot(df.index, df['percentage_14day']
+         ['60304'], color='#33EE33', label="60304")
 plt.ylabel("% Positive")
 plt.legend(loc='best')
 plt.legend().get_frame().set_linewidth(0.0)
@@ -184,14 +197,14 @@ current_value_60301 = df.tail(1)['percentage_14day']['60301'][0]
 current_value_60302 = df.tail(1)['percentage_14day']['60302'][0]
 current_value_60304 = df.tail(1)['percentage_14day']['60304'][0]
 
-plt.title('Oak Park Daily Positive % - '+'{:%b %-d} - 60301: {:0.1f}%  60302: {:0.1f}%  60304: {:0.1f}%'.format(current_date, 100* current_value_60301, 100* current_value_60302, 100* current_value_60304))
+plt.title('Oak Park Daily Positive % - '+'{:%b %-d} - 60301: {:0.1f}%  60302: {:0.1f}%  60304: {:0.1f}%'.format(
+    current_date, 100 * current_value_60301, 100 * current_value_60302, 100 * current_value_60304))
 plt.grid(axis='y', linewidth=0.5)
-plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d')) 
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d'))
 
 # Change the tick interval
-plt.gca().xaxis.set_major_locator(mdates.MonthLocator(interval=1)) 
-plt.gca().yaxis.set_major_formatter(mtick.PercentFormatter(1.0)) 
+plt.gca().xaxis.set_major_locator(mdates.MonthLocator(interval=1))
+plt.gca().yaxis.set_major_formatter(mtick.PercentFormatter(1.0))
 plt.ylim(0)
 plt.savefig('charts/Oak Park Positive Pct by Zip.png', bbox_inches='tight')
 plt.close()
-
--- zipcode.py
+++ zipcode.py
@@ -7,16 +7,20 @@ import asyncio
 import aiohttp
 from aiohttp import ClientSession, ClientConnectorError
 
-age_groups = ['Unknown', '<20', '20-29', '30-39', '40-49', '50-59', '60-69', '70-79', '80+']
+age_groups = ['Unknown', '<20', '20-29', '30-39',
+              '40-49', '50-59', '60-69', '70-79', '80+']
 
 zip_demographics = {}
-async def fetch_html(zip:str, session: ClientSession, **kwargs) -> tuple:
+
+
+async def fetch_html(zip: str, session: ClientSession, **kwargs) -> tuple:
     url = "https://idph.illinois.gov/DPHPublicInformation/api/COVID/GetZipDemographics?zipCode="+zip
     try:
         resp = await session.request(method="GET", url=url, **kwargs)
     except ClientConnectorError as err:
         return (zip, 404)
     return (zip, await resp.json())
+
 
 async def make_requests(zips, **kwargs) -> None:
     async with ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
@@ -28,13 +32,15 @@ async def make_requests(zips, **kwargs) -> None:
         results = await asyncio.gather(*tasks)
 
     for result in results:
-        zip_demographics[result[0]] = result[1] 
+        zip_demographics[result[0]] = result[1]
 
-#fetch the latest file and save it
-r = requests.get("https://idph.illinois.gov/DPHPublicInformation/api/COVID/GetZip")
-zip_json= r.text
+# fetch the latest file and save it
+r = requests.get(
+    "https://idph.illinois.gov/DPHPublicInformation/api/COVID/GetZip")
+zip_json = r.text
 data = json.loads(zip_json)
-file_date = datetime.datetime(data['lastUpdatedDate']['year'], data['lastUpdatedDate']['month'], data['lastUpdatedDate']['day'])
+file_date = datetime.datetime(data['lastUpdatedDate']['year'],
+                              data['lastUpdatedDate']['month'], data['lastUpdatedDate']['day'])
 
 asyncio.run(make_requests(data['zip_values']))
 for zip_data in data['zip_values']:
@@ -50,26 +56,28 @@ for zip_data in data['zip_values']:
             # Get the existing group if available, zero reports are excluded
             group = zip_age_groups[age_group]
             # Rebuild the object because they have spaces appended to group names
-            group = {"age_group": age_group, "count": group['count'], "tested":  group['tested'] }
+            group = {"age_group": age_group,
+                     "count": group['count'], "tested":  group['tested']}
         except:
             # Fake a record to match the old file format
-            group = {"age_group": age_group, "count": 0, "tested": 0 }
+            group = {"age_group": age_group, "count": 0, "tested": 0}
 
         groups.append(group)
- 
+
     zip_data['demographics'] = zip_demographics[zip_data['zip']]
     zip_data['demographics']['age'] = groups
 
 # file_date = datetime.datetime.now()
 file_name = file_date.strftime("%m-%d") + ".json"
 f = open(file_name, "w")
-json.dump(data,f)
+json.dump(data, f)
 f.close()
 
-startdate=datetime.datetime(2020,4,19,0,0,0,0)
+startdate = datetime.datetime(2020, 4, 19, 0, 0, 0, 0)
 target_zips = ['60301', '60302', '60304']
 #target_zips = ['60402']
-population = pd.DataFrame({'zip':target_zips, 'population':[ 2536.00, 32048, 17641]}).set_index('zip')
+population = pd.DataFrame({'zip': target_zips, 'population': [
+                          2536.00, 32048, 17641]}).set_index('zip')
 #population = pd.DataFrame({'zip':target_zips, 'population':[ 63448]}).set_index('zip')
 date = startdate
 df = pd.DataFrame()
@@ -109,20 +117,22 @@ while ((datetime.datetime.now() - date).days >= 0):
         last_good_file = file
         bad_file = False
     except:
-        print('No such file: '+ file)
+        print('No such file: ' + file)
         bad_file = True
 
     if (bad_file == False or last_good_file):
         data = json.load(open(last_good_file))
         try:
-            fileDate = datetime.datetime(data['LastUpdateDate']['year'], data['LastUpdateDate']['month'], data['LastUpdateDate']['day'])
+            fileDate = datetime.datetime(
+                data['LastUpdateDate']['year'], data['LastUpdateDate']['month'], data['LastUpdateDate']['day'])
         except:
-            fileDate = datetime.datetime(data['lastUpdatedDate']['year'], data['lastUpdatedDate']['month'], data['lastUpdatedDate']['day'])
+            fileDate = datetime.datetime(
+                data['lastUpdatedDate']['year'], data['lastUpdatedDate']['month'], data['lastUpdatedDate']['day'])
 
-        strdate= date.strftime("%m-%d")
+        strdate = date.strftime("%m-%d")
         for zip_data in data['zip_values']:
             if (bad_file):
-                table['zip'].append( zip_data['zip'])
+                table['zip'].append(zip_data['zip'])
                 table['date'].append(date)
                 table['count'].append(np.NaN)
                 table['7daypos100k'].append(np.NaN)
@@ -141,7 +151,7 @@ while ((datetime.datetime.now() - date).days >= 0):
                     table[age_group + " percentage_14day"] = 0
 
             else:
-                table['zip'].append( zip_data['zip'])
+                table['zip'].append(zip_data['zip'])
                 table['date'].append(date)
                 table['count'].append(zip_data['confirmed_cases'])
                 table['tested'].append(zip_data['total_tested'])
@@ -161,11 +171,15 @@ while ((datetime.datetime.now() - date).days >= 0):
                 if (demographics_exist):
                     age_index = 0
                     for age_group in zip_data['demographics']['age']:
-                        table[age_groups[age_index] + " count"].append(age_group['count'])
-                        table[age_groups[age_index] + " tested"].append(age_group['tested'])
+                        table[age_groups[age_index] +
+                              " count"].append(age_group['count'])
+                        table[age_groups[age_index] +
+                              " tested"].append(age_group['tested'])
                         table[age_groups[age_index] + " percentage"] = 0
-                        table[age_groups[age_index] + " count_14day"].append(np.NaN)
-                        table[age_groups[age_index] + " tested_14day"].append(np.NaN)
+                        table[age_groups[age_index] +
+                              " count_14day"].append(np.NaN)
+                        table[age_groups[age_index] +
+                              " tested_14day"].append(np.NaN)
                         table[age_groups[age_index] + " percentage_14day"] = 0
                         age_index = age_index + 1
                 else:
@@ -177,15 +191,13 @@ while ((datetime.datetime.now() - date).days >= 0):
                         table[age_group + " tested_14day"].append(np.NaN)
                         table[age_group + " percentage_14day"] = 0
 
-
-    
-    date = date + datetime.timedelta(days= 1)
+    date = date + datetime.timedelta(days=1)
 
 df = pd.DataFrame(table)
 df = df[df['zip'].isin(target_zips)]
 df['date'] = pd.to_datetime(df['date'])
 df = df.join(population, on='zip')
-df2 = df.pivot(index='date',columns='zip')
+df2 = df.pivot(index='date', columns='zip')
 df2 = df2.interpolate()
 diff = pd.DataFrame(df2)
 diff['count'] = diff['count'].diff(periods=1)
@@ -195,13 +207,15 @@ diff['tested'] = diff['tested'].round()
 
 
 for age_group in age_groups:
-    diff[age_group+' count']  = diff[age_group+' count'].diff(periods=1)
-    diff[age_group+' tested']  = diff[age_group+' tested'].diff(periods=1)
+    diff[age_group+' count'] = diff[age_group+' count'].diff(periods=1)
+    diff[age_group+' tested'] = diff[age_group+' tested'].diff(periods=1)
 
-diff['7daypos100k'] = 100000 * diff['count'].rolling(window=7).sum() / diff['population']
+diff['7daypos100k'] = 100000 * \
+    diff['count'].rolling(window=7).sum() / diff['population']
 diff['percentage'] = diff['count'] / diff['tested']
 for age_group in age_groups:
-    diff[age_group+' percentage']  = diff[age_group+' count'] / diff[age_group+' tested']
+    diff[age_group+' percentage'] = diff[age_group +
+                                         ' count'] / diff[age_group+' tested']
 
 
 diff['count_14day'] = diff['count'].rolling(window=14).mean()
@@ -210,10 +224,12 @@ diff['percentage_14day'] = diff['count_14day'] / diff['tested_14day']
 diff['7daypos100k_14day'] = diff['7daypos100k'].rolling(window=14).mean()
 
 for age_group in age_groups:
-    diff[age_group+' percentage']  = diff[age_group+' count'] / diff[age_group+' tested']
+    diff[age_group+' percentage'] = diff[age_group +
+                                         ' count'] / diff[age_group+' tested']
     diff[age_group+' count_14day'] = diff['count'].rolling(window=14).mean()
     diff[age_group+' tested_14day'] = diff['tested'].rolling(window=14).mean()
-    diff[age_group+' percentage_14day']  = diff[age_group+' count_14day'] / diff[age_group+' tested_14day']
+    diff[age_group+' percentage_14day'] = diff[age_group +
+                                               ' count_14day'] / diff[age_group+' tested_14day']
 
 diff.to_csv('zip_detail_all.csv')
 
@@ -223,44 +239,56 @@ dftotals = dftotals.transpose()
 
 dftotals['percentage'] = dftotals['count'] / dftotals['tested']
 for age_group in age_groups:
-    dftotals[age_group+' percentage']  = dftotals[age_group+' count'] / dftotals[age_group+' tested']
+    dftotals[age_group+' percentage'] = dftotals[age_group +
+                                                 ' count'] / dftotals[age_group+' tested']
 
 dftotals['count_14day'] = dftotals['count'].rolling(window=14).mean()
 dftotals['tested_14day'] = dftotals['tested'].rolling(window=14).mean()
-dftotals['percentage_14day'] = dftotals['count_14day'] / dftotals['tested_14day']
-dftotals['7daypos100k'] = 100000 * dftotals['count'].rolling(window=7).sum() / dftotals['population']
-dftotals['7daypos100k_14day'] = dftotals['7daypos100k'].rolling(window=14).mean()
+dftotals['percentage_14day'] = dftotals['count_14day'] / \
+    dftotals['tested_14day']
+dftotals['7daypos100k'] = 100000 * \
+    dftotals['count'].rolling(window=7).sum() / dftotals['population']
+dftotals['7daypos100k_14day'] = dftotals['7daypos100k'].rolling(
+    window=14).mean()
 
 for age_group in age_groups:
-    dftotals[age_group+' percentage']  = dftotals[age_group+' count'] / dftotals[age_group+' tested']
-    dftotals[age_group+' count_14day'] = dftotals[age_group+' count'].rolling(window=14).mean()
-    dftotals[age_group+' tested_14day'] = dftotals[age_group+' tested'].rolling(window=14).mean()
-    dftotals[age_group+' percentage_14day']  = dftotals[age_group+' count_14day'] / dftotals[age_group+' tested_14day']
+    dftotals[age_group+' percentage'] = dftotals[age_group +
+                                                 ' count'] / dftotals[age_group+' tested']
+    dftotals[age_group+' count_14day'] = dftotals[age_group +
+                                                  ' count'].rolling(window=14).mean()
+    dftotals[age_group+' tested_14day'] = dftotals[age_group +
+                                                   ' tested'].rolling(window=14).mean()
+    dftotals[age_group+' percentage_14day'] = dftotals[age_group +
+                                                       ' count_14day'] / dftotals[age_group+' tested_14day']
 
 dftotals.to_csv('zip_rollup_all.csv')
 
 population = pd.read_csv('cook_zipcodes.csv').set_index('zip')
 
 keys = ['zip', 'date', 'count', 'tested']
-subset =  {key: table[key] for key in keys }
+subset = {key: table[key] for key in keys}
 subset['cases_per_million_14day'] = subset['count']
 df = pd.DataFrame(subset)
 # df = df[df['zip'].isin(population.index)]
 df['zip'] = df['zip'].astype('int')
 df['date'] = pd.to_datetime(df['date'])
-df = df.pivot(index='date',columns='zip').interpolate()
+df = df.pivot(index='date', columns='zip').interpolate()
 
-count = df['count'].reset_index().melt(id_vars=('date'), value_name='count').set_index(keys=['date','zip']).join(population, on='zip', how='inner')
-tested = df['tested'].reset_index().melt(id_vars=('date'), value_name='tested').set_index(keys=['date','zip']).join(population, on='zip', how='inner')
+count = df['count'].reset_index().melt(id_vars=('date'), value_name='count').set_index(
+    keys=['date', 'zip']).join(population, on='zip', how='inner')
+tested = df['tested'].reset_index().melt(id_vars=('date'), value_name='tested').set_index(
+    keys=['date', 'zip']).join(population, on='zip', how='inner')
 out = count.join(tested, rsuffix='_')
-df = pd.DataFrame( out, columns=['zip','city','count','tested','population'])
-keys = [ '7daypos100k', 'percentage', 'count_14day', 'tested_14day', 'percentage_14day', '7daypos100k_14day', 'cases_per_million_14day']
+df = pd.DataFrame(out, columns=['zip', 'city',
+                                'count', 'tested', 'population'])
+keys = ['7daypos100k', 'percentage', 'count_14day', 'tested_14day',
+        'percentage_14day', '7daypos100k_14day', 'cases_per_million_14day']
 for key in keys:
     df[key] = 0
 df = df[df['population'] > 0]
 df = df.groupby(by=['city', 'date']).sum()
 df = df.reset_index()
-df2 = df.pivot(index='date',columns='city')
+df2 = df.pivot(index='date', columns='city')
 diff = pd.DataFrame(df2)
 diff['count'] = diff['count'].diff(periods=1)
 diff['tested'] = diff['tested'].diff(periods=1)
@@ -268,21 +296,22 @@ diff['count'] = diff['count'].round()
 diff['tested'] = diff['tested'].round()
 
 
-diff['7daypos100k'] = 100000 * diff['count'].rolling(window=7).sum() / diff['population']
+diff['7daypos100k'] = 100000 * \
+    diff['count'].rolling(window=7).sum() / diff['population']
 diff['percentage'] = diff['count'] / diff['tested']
 
 
 diff['count_14day'] = diff['count'].rolling(window=14).mean()
-diff['cases_per_million_14day'] = 1000000*diff['count_14day'] / diff['population'] 
+diff['cases_per_million_14day'] = 1000000 * \
+    diff['count_14day'] / diff['population']
 diff['tested_14day'] = diff['tested'].rolling(window=14).mean()
 diff['percentage_14day'] = diff['count_14day'] / diff['tested_14day']
 diff['7daypos100k_14day'] = diff['7daypos100k'].rolling(window=14).mean()
 
 diff.to_csv('zip_city_all.csv')
 
-diff['cases_per_million_14day'].tail(1).transpose().to_csv('zip_city_cases_per_million_latest.csv')
-diff['percentage_14day'].tail(1).transpose().to_csv('zip_city_percentage_latest.csv')
+diff['cases_per_million_14day'].tail(1).transpose().to_csv(
+    'zip_city_cases_per_million_latest.csv')
+diff['percentage_14day'].tail(1).transpose().to_csv(
+    'zip_city_percentage_latest.csv')
 diff['count_14day'].tail(1).transpose().to_csv('zip_city_cases_latest.csv')
-
-
-
